fdd85f8b76f85205506d0cbab25cd85bb738deea
Cédric B <cedricbardaine@gmail.com>
2020-03-03 19:33:51 +0100

    explain deprecating of InvalidSizeItem

--- src/com/tactfactory/designpattern/controle/exceptions/InvalidSizeItem.java
+++ src/com/tactfactory/designpattern/controle/exceptions/InvalidSizeItem.java
@@ -1,5 +1,13 @@
 package com.tactfactory.designpattern.controle.exceptions;
 
+import com.tactfactory.designpattern.controle.entities.Item;
+
+/**
+ * 
+ * @author cedri
+ * @deprecated was used to verify the size of {@link Item}s but the Interface doesn't permit to easily throw Exceptions.
+ *
+ */
 public class InvalidSizeItem extends Exception{
 	public InvalidSizeItem(String errMsg) {
 		super(errMsg) ; 
